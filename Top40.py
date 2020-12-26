c46e4564e2efd8c3a69be8dfffc6fd7e994b193a
Anthony I <a.iacampo99@gmail.com>
2020-12-26 18:48:02 -0500

    Final commit
    Co-authored-by: Caleb Lim <caleb.lim@mail.mcgill.ca>
    Co-authored-by: Jordan Hum <jordan.hum@mail.concordia.ca>

--- Top40.py
+++ Top40.py
@@ -2,65 +2,147 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import urllib.request
 from openpyxl import load_workbook
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 
-url = 'https://www.at40.com/charts/top-40-238/2020-12-12-december-12-2020-0'
 
-source = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(source, 'lxml')
+# ---- Function call ----
 
-# Get date
-title = soup.title.string.split(" ")
-year = title[5]
-date = title[3] + " " + title[4] + " " + year
+# Scrape song metadata from website and saves list into Excel file
+def scrapeSongs():
+    url = entry.get().strip()
+    path = pathEntry.get()
 
-# Get Excel file
-try:
-    xlsx = pd.ExcelFile('AT40 songs.xlsx')
-    df = pd.read_excel(xlsx, year)
-    sheetExist = True
-except:
-    sheetExist = False
-    df = pd.DataFrame([], columns=['Song', 'Artist',
-                                   'Date Added', 'Downloaded'])
+    # Validate input
+    if not url:
+        messagebox.showwarning('Warning', 'Please enter a url')
+        return
 
-dfSheets = pd.concat(pd.read_excel(xlsx, sheet_name=None), ignore_index=True)
+    if not 'www.at40.com/charts/' in url:
+        messagebox.showwarning('Warning', 'Please enter a url from AT40')
+        return
 
-if dfSheets.empty:
-    dfSheets = pd.DataFrame([], columns=['Song', 'Artist',
-                                         'Date Added', 'Downloaded'])
+    if not path:
+        messagebox.showwarning('Warning', 'Please import a file')
+        return
 
-# Scraping
-tracks = soup.find(
-    'div', class_='component-container component-chartlist block').find_all('figcaption')
+    if not path.endswith('.xlsx'):
+        messagebox.showwarning(
+            'Warning', 'Please import an Excel file (.xlsx)')
+        return
 
-# Get songs, check for duplicates & append to Excel file
-newSongs = []
-for track in tracks:
     try:
-        song = track.find('a', class_='track-title').string
-        artist = track.find('a', class_='track-artist').string
+        source = urllib.request.urlopen(url).read()
+        soup = BeautifulSoup(source, 'lxml')
     except:
-        song = track.find('span', class_='track-title').string
-        artist = track.find('span', class_='track-artist').string
-    else:
-        pass
+        messagebox.showerror(
+            'Error', "This site can't be reached")
+        return
 
-    if not ((dfSheets['Song'] == song) & (dfSheets['Artist'] == artist)).any():
-        newSongs.append(
-            pd.Series([song, artist, date, None], index=df.columns))
+    # Get date
+    title = soup.title.string.split(" ")
+    year = title[5]
+    date = title[3] + " " + title[4] + " " + year
 
-newDf = df.append(newSongs, ignore_index=True)
+    # Get Excel file
+    try:
+        xlsx = pd.ExcelFile(path)
+        df = pd.read_excel(xlsx, year)
+        sheetExist = True
 
-# Write to Excel file
-try:
-    with pd.ExcelWriter('AT40 songs.xlsx') as writer:
-        writer.book = load_workbook('AT40 songs.xlsx')
-        if sheetExist:
-            del writer.book[year]
-        newDf.to_excel(writer, sheet_name=year, index=False)
-        writer.book.save('AT40 songs.xlsx')
-except:
-    print('Error: Excel file must be closed for the program to execute\nClose the excel file and run the program again')
-    exit(0)
+        if not all([item in df.columns for item in ['Song', 'Artist', 'Date Added', 'Downloaded']]):
+            df = pd.DataFrame([], columns=['Song', 'Artist',
+                                           'Date Added', 'Downloaded'])
+    except:
+        sheetExist = False
+        df = pd.DataFrame([], columns=['Song', 'Artist',
+                                       'Date Added', 'Downloaded'])
 
-print(newDf)
+    try:
+        dfSheets = pd.concat(pd.read_excel(
+            xlsx, sheet_name=None), ignore_index=True)
+    except:
+        messagebox.showerror('Error', "File path not found")
+        return
+
+    if dfSheets.empty:
+        dfSheets = pd.DataFrame([], columns=['Song', 'Artist',
+                                             'Date Added', 'Downloaded'])
+
+    # Scraping
+    try:
+        tracks = soup.find(
+            'div', class_='component-container component-chartlist block').find_all('figcaption')
+    except:
+        messagebox.showerror(
+            'Error', 'Please enter a url from the weekly charts')
+        return
+
+    # Get songs, check for duplicates & append to Excel file
+    newSongs = []
+    for track in tracks:
+        try:
+            song = track.find('a', class_='track-title').string
+            artist = track.find('a', class_='track-artist').string
+        except:
+            song = track.find('span', class_='track-title').string
+            artist = track.find('span', class_='track-artist').string
+        else:
+            pass
+
+        if not ((dfSheets['Song'] == song) & (dfSheets['Artist'] == artist)).any():
+            newSongs.append(
+                pd.Series([song, artist, date, None], index=df.columns))
+
+    newDf = df.append(newSongs, ignore_index=True)
+
+    # Write to Excel file
+    try:
+        with pd.ExcelWriter(path) as writer:
+            writer.book = load_workbook(path)
+            if sheetExist:
+                del writer.book[year]
+            newDf.to_excel(writer, sheet_name=year, index=False)
+            writer.book.save(path)
+    except:
+        messagebox.showerror(
+            'Error', "Excel file must be closed for the program to execute")
+        return
+
+    messagebox.showinfo("Done", "Chart has been successfully scraped")
+
+
+# Get path to Excel file and writes path in Entry Field
+
+def UploadAction(event=None):
+    filename = filedialog.askopenfilename()
+    pathEntry.configure(state="normal")
+    pathEntry.delete(0, tk.END)
+    pathEntry.insert(0, filename)
+    pathEntry.configure(state="disabled")
+
+
+# ------ GUI --------
+win = tk.Tk()
+
+tk.Label(win, text="Enter URL").grid(row=0)
+win.title("Top40 Scraper")
+win.geometry("500x100")
+win.resizable(False, False)
+
+entry = tk.Entry(win, width=71)
+entry.grid(row=0, column=1, padx=5, pady=5)
+
+pathEntry = tk.Entry(win, width=71)
+pathEntry.grid(row=1, column=1, padx=5, pady=5)
+pathEntry.configure(state="disabled")
+
+tk.Button(win, text="Import", command=UploadAction
+          ).grid(row=1, column=0, sticky=tk.W, padx=5, pady=5)
+tk.Button(win, text="Scrape", command=scrapeSongs
+          ).grid(row=2, column=1, sticky=tk.W, padx=5, pady=5)
+tk.Button(win, text="Quit", command=win.quit).grid(
+    row=2, column=1, sticky=tk.W, padx=60, pady=5)
+
+win.mainloop()
